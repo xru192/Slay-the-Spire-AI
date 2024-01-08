dbaefe14a0a03e6479c0d19e135735c9c8b40d3b
xru192 <rx26@cornell.edu>
2024-01-08 15:38:41 -0500

    Improve rest site AI

---
+++ src/main/java/newaimod/AI/AbstractRestAutoPlayer.java
@@ -0,0 +1,94 @@
+package newaimod.AI;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.RestRoom;
+import communicationmod.ChoiceScreenUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+
+/**
+ * An AbstractRestAutoPlayer implementation progresses the game at a rest site.
+ */
+public abstract class AbstractRestAutoPlayer {
+    public static final Logger logger = LogManager.getLogger(AbstractRestAutoPlayer.class.getName());
+
+    protected enum RESTCHOICE {
+        REST, SMITH, LIFT, TOKE, DIG, RECALL;
+
+        @Override
+        public String toString() {
+            return this.name().toLowerCase();
+        }
+    }
+
+    public void execute() {
+        logger.info("Executing...");
+        if (ChoiceScreenUtils.getCurrentChoiceType() != ChoiceScreenUtils.ChoiceType.REST) {
+            logger.info("Done: choice type not suitable");
+            return;
+        }
+        ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList();
+        logger.info("Choosing between: " + choices);
+        if (ChoiceScreenUtils.isConfirmButtonAvailable()) {
+            logger.info("Pressing confirm");
+            ChoiceScreenUtils.pressConfirmButton();
+            logger.info("Done");
+            return;
+        }
+        if (((RestRoom) AbstractDungeon.getCurrRoom()).campfireUI.somethingSelected) {
+            logger.info("Done: something was already selected");
+            return;
+        }
+        if (choices.size() == 1) {
+            ChoiceScreenUtils.makeRestRoomChoice(0);
+        }
+
+        boolean canRest = choices.contains("rest");
+        boolean canSmith = choices.contains("smith");
+        boolean canLift = choices.contains("lift");
+        boolean canToke = choices.contains("toke");
+        boolean canDig = choices.contains("dig");
+        boolean canRecall = choices.contains("recall");
+
+        RESTCHOICE choice = choose(canRest, canSmith, canLift, canToke, canDig, canRecall);
+        boolean chosen = choose(choice);
+        if (chosen) {
+            logger.info("Chose " + choice);
+            logger.info("Done");
+        } else {
+            logger.error("AbstractRestAutoPlayer implementation chose an invalid rest site option.");
+        }
+
+    }
+
+    /**
+     * Returns a choice to perform at the current rest site. Only returns an available option. Precondition: at least 2
+     * options are available.
+     *
+     * @param canRest   whether "Rest" is an available option
+     * @param canSmith  whether "Smith" is an available option
+     * @param canLift   whether "Lift" is an available option
+     * @param canToke   whether "Toke" is an available option
+     * @param canDig    whether "Dig" is an available option
+     * @param canRecall whether "Recall" is an available option
+     * @return choice to perform at the current rest site
+     */
+    protected abstract RESTCHOICE choose(boolean canRest, boolean canSmith, boolean canLift, boolean canToke, boolean canDig, boolean canRecall);
+
+    private static boolean choose(RESTCHOICE choice) {
+        try {
+            ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList();
+            if (!choices.contains(choice.toString())) {
+                return false;
+            }
+            ChoiceScreenUtils.makeRestRoomChoice(choices.indexOf(choice.toString()));
+            return true;
+        } catch (Exception e) {
+            logger.info("Failed to make choice: " + choice + ". Error: " + e.getMessage());
+            return false;
+        }
+    }
+
+}
--- src/main/java/newaimod/AI/BasicIroncladPlayer/BasicIroncladRestPlayer.java
+++ src/main/java/newaimod/AI/BasicIroncladPlayer/BasicIroncladRestPlayer.java
@@ -1,53 +1,36 @@
 package newaimod.AI.BasicIroncladPlayer;
 
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.rooms.RestRoom;
-import communicationmod.ChoiceScreenUtils;
+import newaimod.AI.AbstractRestAutoPlayer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-
-public class BasicIroncladRestPlayer {
+public class BasicIroncladRestPlayer extends AbstractRestAutoPlayer {
     public static final Logger logger = LogManager.getLogger(BasicIroncladRestPlayer.class.getName());
 
-    /**
-     * Rest if health is below 60, upgrade otherwise.
-     */
-    public void execute() {
-        logger.info("Executing...");
-        if (ChoiceScreenUtils.getCurrentChoiceType() != ChoiceScreenUtils.ChoiceType.REST) {
-            logger.info("Done: choice type not suitable");
-            return;
-        }
-        ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList();
-        logger.info("Choosing between: " + choices.toString());
-        if (ChoiceScreenUtils.isConfirmButtonAvailable()) {
-            logger.info("Pressing confirm");
-            ChoiceScreenUtils.pressConfirmButton();
-            logger.info("Done");
-            return;
-        }
-        if (((RestRoom) AbstractDungeon.getCurrRoom()).campfireUI.somethingSelected) {
-            logger.info("Done: something was already selected");
-            return;
+    @Override
+    protected RESTCHOICE choose(boolean canRest, boolean canSmith, boolean canLift, boolean canToke, boolean canDig, boolean canRecall) {
+        int needRest = 29;
+        if (AbstractDungeon.floorNum == 15) {
+            logger.info("At rest site before Act I boss");
+            needRest = 39;
         }
 
-        // pick the first option
-        if (!choices.isEmpty()) {
-            choose(choices.get(0));
+        if (AbstractDungeon.player.currentHealth <= needRest) {
+            return RESTCHOICE.REST;
         }
-        logger.info("Done");
 
-    }
-
-    public static void choose(String choice) {
-        try {
-            ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList();
-            logger.info("Making choice: " + choice);
-            ChoiceScreenUtils.makeRestRoomChoice(choices.indexOf(choice));
-        } catch (Exception e) {
-            logger.info("Failed to make choice: " + choice + ". Error: " + e.getMessage());
+        if (canLift) {
+            return RESTCHOICE.LIFT;
+        } else if (canSmith) {
+            return RESTCHOICE.SMITH;
+        } else if (canDig) {
+            return RESTCHOICE.DIG;
+        } else if (canToke) {
+            return RESTCHOICE.TOKE;
+        } else {
+            return RESTCHOICE.RECALL;
         }
+
     }
 }
