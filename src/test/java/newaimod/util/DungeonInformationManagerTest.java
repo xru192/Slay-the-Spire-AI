f8892f2f5c4b1f52d86ac486919db7bf97871e2f
xru192 <rx26@cornell.edu>
2024-01-23 22:42:35 -0500

    Update card selection for Act I

---
+++ src/test/java/newaimod/util/DungeonInformationManagerTest.java
@@ -0,0 +1,19 @@
+package newaimod.util;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.red.Strike_Red;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DungeonInformationManagerTest {
+    @Test
+    void testDeckInfo() {
+        ArrayList<AbstractCard> cards = new ArrayList<>();
+        DungeonInformationManager.DeckInfo deckInfo0 = new DungeonInformationManager.DeckInfo(cards);
+        cards.add(null);
+        assertEquals(0, deckInfo0.deckSize());
+    }
+}
